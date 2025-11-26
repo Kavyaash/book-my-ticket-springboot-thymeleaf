ad32048b622b96f50969a183137c2890d725aad5
kavyaash <kavyaashok32@gmail.com>
2025-11-26 19:11:03 +0530

    Project committed

---
+++ src/main/java/com/jsp/book/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.jsp.book.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.book.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	boolean existsByEmail(String email);
+
+	void deleteByRole(String string);
+
+	User findByEmail(String email);
+
+}
---
+++ src/main/java/com/jsp/book/util/EmailHelper.java
@@ -0,0 +1,9 @@
+package com.jsp.book.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailHelper {
+
+
+}
